56af3e22a2ee518d4f71dd4bfcb3925be0f61c25
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-05-22 18:45:55 +0530

    registerd user using api

--- server/src/app.js
+++ server/src/app.js
@@ -7,8 +7,17 @@ const userRoute= require('./routes/users/usersRoutes');
 // using express
 const app = express();
 
+// const logger=(req,res,next) => {
+//     console.log('am a logger');
+//     next();
+// };
+// app.use(logger);
+
 //connecting the db
 dbConnect();
+
+//middlewares
+app.use(express.json());
 
 //middleware to use routes folder
 app.use('/',userRoute);
--- server/src/controllers/users/usersCtrl.js
+++ server/src/controllers/users/usersCtrl.js
@@ -1,6 +1,19 @@
-//Register a new user
-const registerUser=(req,res) =>{
-    res.json({user:'admin'});
-};
+const User=require('../../model/User');
 
+//Register a new user
+const registerUser= async (req,res) =>{
+    const {email,firstname,lastname,password} = req?.body;
+   try {
+   //check if user exists already
+   const userExists=await User.findOne({email});
+   
+   if(userExists){
+       res.json('User already exists');
+   }
+   const user=await User.create({email,firstname,lastname,password});
+   res.status(200).json(user);
+   }catch(error){
+      res.json(error);
+   }
+};
 module.exports = registerUser;
